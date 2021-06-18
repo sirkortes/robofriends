c70fc4ca89492c5e4099806b5347f0bdcfbcc4b3
sirkriz <kriz@krizcortes.com>
2021-06-17 23:09:10 -0400

    regular and async tests, and mocks / spies on async tests

--- test/script.test.js
+++ test/script.test.js
@@ -3,11 +3,22 @@ const googleSearch = require('./script')
 /* you wouldn't import db here, you'd mock it */
 dbMock = ['dog.com', 'cheesepuff.com', 'disney.com', 'dogpictures.com']
 
-it('this is a test', () => {
-  expect('hello').toBe('hello')
-})
+describe('googleSearch', () => {
+  it('this is a test', () => {
+    expect('hello').toBe('hello')
+  })
 
-it('is searching google', () => {
-  expect(googleSearch('testest', dbMock)).toEqual([])
-  expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com'])
+  it('is searching google', () => {
+    expect(googleSearch('testest', dbMock)).toEqual([])
+    expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com'])
+  })
+
+  it('works with undefined and null input', () => {
+    expect(googleSearch(undefined, dbMock)).toEqual([])
+    expect(googleSearch(null, dbMock)).toEqual([])
+  })
+
+  it('does not return more than 3 matches', () => {
+    expect(googleSearch('.com', dbMock).length).toBe(3)
+  })
 })
