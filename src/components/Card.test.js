088afa7d88bfc975a3a90acee37922f654c8e5e0
sirkriz <kriz@krizcortes.com>
2021-06-18 00:02:54 -0400

    implemented enzyme shallow and snapshot tests on components and ran coverage reports

---
+++ src/components/Card.test.js
@@ -0,0 +1,8 @@
+import { shallow } from 'enzyme'
+import React from 'react'
+import Card from './Card'
+
+it('expect to render Card component', () => {
+  // expect(shallow(<Card />).length).toEqual(1)
+  expect(shallow(<Card />)).toMatchSnapshot()
+})
